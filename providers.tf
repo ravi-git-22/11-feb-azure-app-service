a1a663f409eedd0f0559b93761a85fafe0ccf283
ravi-git-22 <ravikiran.thames@outlook.com>
2023-02-11 15:58:14 +0000

    commit-3: modified providers.tf

--- providers.tf
+++ providers.tf
@@ -9,6 +9,6 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "35b269d2-7c5c-4f36-abf1-56099fd97991"
-  tenant_id       = "1df39095-a0f4-4b9f-abcf-e96d3abd9282"
+  #subscription_id = "35b269d2-7c5c-4f36-abf1-56099fd97991"
+  #tenant_id       = "1df39095-a0f4-4b9f-abcf-e96d3abd9282"
 }
